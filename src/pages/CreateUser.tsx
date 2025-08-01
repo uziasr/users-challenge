3a84f0e32d9285210ed1c97d7e13bad8d5da99d0
UZIAS RIVERA <uziasr@g.ucla.edu>
2025-08-01 16:51:28 -0700

    created the Create User page

---
+++ src/pages/CreateUser.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { CreateUserForm } from "../features/user/CreateUserForm";
+export const CreateUser = () => {
+  return (
+    <div>
+      <h1>Create User</h1>
+      <CreateUserForm />
+    </div>
+  );
+};
